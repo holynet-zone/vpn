4f7e09a3af157b6e7d9b9878730b655acd22aee5
JKearnsl <boyarshinovn@gmail.com>
2024-09-13 20:35:12 +0300

    update: added simple udp hello world

--- src/client/src/main.rs
+++ src/client/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::UdpSocket;
+
+fn main()  -> std::io::Result<()> {
+    let socket = UdpSocket::bind("127.0.0.1:3400")?;
+    socket.connect("127.0.0.1:34254")?;
+    
+    socket.send(b"hello world!")?;
+    
+    let mut buf = [0; 1024];
+    let (amt, src) = socket.recv_from(&mut buf)?;
+    
+    println!("{} bytes received from {:?}", amt, src);
+    println!("{:?}", &buf[..amt]);
+    
+    Ok(())
 }
--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::UdpSocket;
+
+fn main() -> std::io::Result<()> {
+    {
+        let socket = UdpSocket::bind("127.0.0.1:34254")?;
+        
+        let mut buffer = [0; 1024];
+        let (amt, src) = socket.recv_from(&mut buffer)?;
+        print!("Received: ");
+        for i in 0..amt {
+            print!("{}", buffer[i] as char);
+        }
+
+        let buffer = &mut buffer[..amt];
+        buffer.reverse();
+        socket.send_to(buffer, &src)?;
+    }
+    Ok(())
 }
