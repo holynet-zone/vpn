599517047d8fc096b230dc309aa5da3fd50fb8ca
JKearnsl <boyarshinovn@gmail.com>
2024-09-23 15:47:19 +0300

    update: basic client implementation

--- src/client/src/main.rs
+++ src/client/src/main.rs
@@ -1,35 +1,26 @@
 use std::io::{Read, Write};
-use std::net::UdpSocket;
+use std::net::SocketAddr;
 use std::process::Command;
-use std::thread;
+use std::process;
+use std::os::fd::AsRawFd;
 use log::{error, info, warn};
+use mio::{Events, Interest, Poll, Token};
+use mio::net::UdpSocket;
 use tun::platform::Device;
-use serde::{Deserialize, Serialize};
+use common::{Body, DATA_SIZE, is_root, MAX_PACKET_SIZE};
 
-const INTERFACE_NAME_IN: &str = "holynet0";
-const INTERFACE_NAME_OUT: &str = "holynet1";
+const INTERFACE_NAME: &str = "holynet1";
 
-fn setup_in_tun(name: &str) -> Result<Device, String> {
+const UDP: Token = Token(0);
+const TUN: Token = Token(1);
+
+
+fn setup_tun(name: &str, mtu: usize) -> Result<Device, String> {
     let mut config = tun::Configuration::default();
     config.name(name);
-    config.address((10, 8, 0, 1));
+    config.address((10, 8, 0, 2));
     config.netmask((255, 255, 255, 0));
-    config.up();
-    
-
-    let tun_device = tun::create(&config).map_err(|error| {
-        let err_msg = format!("Failed to create the TUN device: {}", error);
-        error!("{}",&err_msg);
-        err_msg
-    })?;
-    Ok(tun_device)
-}
-
-fn setup_out_tun(name: &str) -> Result<Device, String> {
-    let mut config = tun::Configuration::default();
-    config.name(name);
-    config.address((10, 7, 0, 2));
-    config.netmask((255, 255, 255, 0));
+    config.mtu(mtu as i32);
     config.up();
 
     let tun_device = tun::create(&config).map_err(|error| {
@@ -43,7 +34,7 @@ fn setup_out_tun(name: &str) -> Result<Device, String> {
         .arg("add")
         .arg("0.0.0.0/0")
         .arg("via")
-        .arg("10.7.0.1")
+        .arg("10.8.0.1")
         .arg("dev")
         .arg(name)
         .output()
@@ -58,78 +49,86 @@ fn setup_out_tun(name: &str) -> Result<Device, String> {
     Ok(tun_device)
 }
 
-#[derive(Serialize, Deserialize)]
-struct VpnPacket {
-    client_id: u8,
-    data: Vec<u8>
-}
-
-async fn connect(address: &str, client_id: u8) -> Result<(), String> {
-    let mut socket = UdpSocket::bind("127.0.0.1:0").map_err(|error| {
+async fn connect(address: &str) -> Result<(), String> {
+    let server_address: SocketAddr = address.parse().map_err(|error| {
+        error!("Failed to parse the server address: {}", error);
+        process::exit(1);
+    }).unwrap();
+    
+    let mut socket = UdpSocket::bind("127.0.0.1:0".parse().unwrap()).map_err(|error| {
         let err_msg = format!("Failed to bind the client socket: {}", error);
         error!("{}",&err_msg);
         err_msg
     })?;
-    socket.connect(address).map_err(|error| {
+    
+    socket.connect(server_address).map_err(|error| {
         let err_msg = format!("Failed to connect to the server: {}", error);
         error!("{}",&err_msg);
         err_msg
     })?;
+    
+    let mut tun = setup_tun(INTERFACE_NAME, DATA_SIZE)?;
+    let tun_raw_fd = tun.as_raw_fd();
+    let mut tunfd = mio::unix::SourceFd(&tun_raw_fd);
 
-    let mut socket_clone = socket.try_clone().unwrap();
-
-    let mut in_tun_device = setup_in_tun(INTERFACE_NAME_IN)?;
-    let mut out_tun_device = setup_out_tun(INTERFACE_NAME_OUT)?;
+    let mut poll = Poll::new().unwrap();
+    let mut events = Events::with_capacity(1024);
+    poll.registry()
+        .register(&mut socket, UDP, Interest::READABLE).unwrap();
+    poll.registry()
+        .register(&mut tunfd, TUN, Interest::READABLE).unwrap();
 
     info!("Connected to the server {}", address);
 
-    thread::scope(|s| {
-        s.spawn(|| {
-            let mut buffer = [0u8; 1500];
-            loop {
-                match out_tun_device.read(&mut buffer) {
-                    Ok(n) => {
-                        info!("{} Received from the tun device", n);
-                        let vpn_packet = VpnPacket {
-                            client_id,
-                            data: buffer[0..n].to_vec(),
-                        };
-                        buffer.fill(0);
-                        let encoded: Vec<u8> = bincode::serialize(&vpn_packet).unwrap();
-                        socket.send(&encoded).unwrap();
-                    }
-                    Err(error) => {
-                        warn!("Error reading from tun device: {}", error);
-                        continue;
-                    }
-                }
-            }
-        });
-
-        s.spawn(|| {
-            let mut buffer = [0u8; 1500];
-            loop {
-                match socket_clone.recv(&mut buffer) {
-                    Ok(n) => {
-                        info!("{} Received from the server", n);
-                        let vpn_packet: VpnPacket = bincode::deserialize(&buffer[..n]).unwrap();
-                        info!("Writing data to tun0");
-                        in_tun_device.write(&vpn_packet.data).unwrap();
-                    }
-                    Err(e) => {
-                        error!("Error reading from client: {}", e);
-                        continue;
+    loop {
+        poll.poll(&mut events, None).unwrap();
+        for event in events.iter() {
+            match event.token() {
+                UDP => {
+                    let mut buffer = [0u8; MAX_PACKET_SIZE];
+                    match socket.recv(&mut buffer) {
+                        Ok(n) => {
+                            info!("{} Received from the server", n);
+                            let packet = Body::Data { data: buffer[0..n].to_vec() };
+                            info!("Writing data to tun0");
+                            tun.write(&packet.to_bytes().unwrap()).unwrap();
+                        }
+                        Err(e) => {
+                            error!("Error reading from client: {}", e);
+                            continue;
+                        }
                     }
                 }
+                TUN => {
+                    let mut buffer = [0u8; MAX_PACKET_SIZE];
+                    match tun.read(&mut buffer) {
+                        Ok(n) => {
+                            info!("{} Received from the tun device", n);
+                            let packet = Body::Data { data: buffer[0..n].to_vec() };
+                            buffer.fill(0);
+                            socket.send(&packet.to_bytes().unwrap()).unwrap();
+                        }
+                        Err(error) => {
+                            warn!("Error reading from tun device: {}", error);
+                            continue;
+                        }
+                    }
+                }
+                _ => unreachable!(),
             }
-        });
-    });
-    Ok(())
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init();
-    connect("127.0.0.1:34254", 1).await.unwrap();
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+
+    if !is_root() {
+        error!("This program must be run as root");
+        process::exit(1);
+    }
+    
+    connect("92.255.67.65:34254").await.unwrap();
     Ok(())
 }
