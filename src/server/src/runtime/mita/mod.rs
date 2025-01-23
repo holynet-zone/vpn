4ef2aaf63df586ae4cb2dee25bd393abac36d592
JKearnsl <boyarshinovn@gmail.com>
2025-01-23 13:06:08 +0300

    update: ch signal type

--- src/server/src/runtime/mita/mod.rs
+++ src/server/src/runtime/mita/mod.rs
@@ -7,7 +7,7 @@ use std::sync::atomic::{AtomicBool, Ordering};
 use std::time::{Duration, Instant};
 use mio::{Events, Interest, Poll, Token};
 use mio::net::UdpSocket;
-use tracing::{error, info, warn};
+use tracing::{debug, error, info, warn};
 use crate::session::single::Sessions;
 use crate::runtime::base::{Configurable, Run, Stop};
 use tun::{set_ipv4_forwarding, setup_tun};
@@ -27,15 +27,15 @@ const TUN_BUFFER_SIZE: usize = 65536;
 
 
 pub struct MitaRunner {
-    stop_tx: Arc<Mutex<Sender<RuntimeError>>>,
-    stop_rx: Arc<Mutex<Receiver<RuntimeError>>>,
+    stop_tx: Option<Sender<RuntimeError>>
 }
 
 impl Run for MitaRunner {
-    fn run(runtime: &Configurable<Self>) {
+    fn run(runtime: &mut Configurable<Self>) {
         info!("Starting the Mita runtime");
-        let stop_tx = runtime.runtime.stop_tx.clone();
-        let stop_rx = runtime.runtime.stop_rx.clone();
+        let (stop_tx, stop_rx) = std::sync::mpsc::channel();
+        runtime.runtime.stop_tx = Some(stop_tx.clone());
+        
         let config = match runtime.config.clone() {
             Some(config) => config,
             None => {
@@ -49,7 +49,7 @@ impl Run for MitaRunner {
 
         thread::spawn(move || {
             loop {
-                if let Some(error) = stop_rx.lock().unwrap().try_recv().ok() {
+                if let Some(error) = stop_rx.try_recv().ok() {
                     info!("Stopping the Mita runtime");
                     stop_flag_clone.store(true, Ordering::Relaxed);
                     error!("{}", error);
@@ -90,7 +90,7 @@ impl Run for MitaRunner {
             set_ipv4_forwarding(true).unwrap();
 
             let mut poll = Poll::new().unwrap();
-            let mut events = Events::with_capacity(config.event_capacity);
+            let mut events = Events::with_capacity(2usize.pow(16));
             println!("events capacity: {}", events.capacity());
             poll.registry()
                 .register(&mut socket, UDP, Interest::READABLE).unwrap();
@@ -114,6 +114,7 @@ impl Run for MitaRunner {
                                 Ok((n, client_addr)) => {
                                     match protocol::ClientPacket::from_bytes(&udp_buffer[0..n]) {
                                         Ok(packet) => {
+                                            let start = Instant::now();
                                             handlers::client_event(
                                                 packet,
                                                 &mut tun,
@@ -123,8 +124,9 @@ impl Run for MitaRunner {
                                                 config.network_prefix,
                                                 &db
                                             ).unwrap_or_else(
-                                                |error| stop_tx.lock().unwrap().send(error).unwrap()
+                                                |error| stop_tx.send(error).unwrap()
                                             );
+                                            debug!("Client udp event took: {:?}", start.elapsed());
                                         },
                                         Err(e) => warn!("Failed to deserialize client packet: {}", e)
                                     }
@@ -136,13 +138,15 @@ impl Run for MitaRunner {
                         TUN => {
                             match tun.read(&mut tun_buffer) {
                                 Ok(n) => {
+                                    let start = Instant::now();
                                     device_event(
                                         &tun_buffer[0..n],
                                         &mut socket,
                                         &clients
                                     ).unwrap_or_else(
-                                        |error| stop_tx.lock().unwrap().send(error).unwrap()
+                                        |error| stop_tx.send(error).unwrap()
                                     );
+                                    debug!("Device event took: {:?}", start.elapsed());
                                 },
                                 Err(e) => warn!("Failed to receive data: {}", e)
                             }
@@ -158,10 +162,9 @@ impl Run for MitaRunner {
 
 impl Stop for MitaRunner {
     fn stop(runtime: &Configurable<Self>) {
-        info!("Stopping the Mita runtime");
-        runtime.runtime.stop_tx.lock().unwrap().send(
-            RuntimeError::StopSignal
-        ).unwrap();
+        if let Some(stop_tx) = runtime.runtime.stop_tx.clone() {
+            stop_tx.send(RuntimeError::StopSignal).unwrap();
+        }
     }
 }
 
@@ -169,12 +172,10 @@ pub type Mita = Configurable<MitaRunner>;
 
 impl Mita {
     pub fn new() -> Self {
-        let (tx, rx) = std::sync::mpsc::channel();
         Mita {
             config: None,
             runtime: MitaRunner {
-                stop_tx: Arc::new(Mutex::new(tx)),
-                stop_rx: Arc::new(Mutex::new(rx)),
+                stop_tx: None
             }
         }
     }
