1bfd192b2e91fa02943bd6275fa74c6762330e1f
JKearnsl <boyarshinovn@gmail.com>
2025-04-06 15:16:59 +0300

    fix

--- src/server/src/network.rs
+++ src/server/src/network.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 use std::net::IpAddr;
 use etherparse::SlicedPacket;
 use pnet::datalink;
+use tun_rs::IntoAddress;
 
 pub fn find_available_ifname(base_name: &str) -> String {
     let interfaces = datalink::interfaces();
@@ -26,12 +27,15 @@ pub fn parse_source(packet: &[u8]) -> anyhow::Result<IpAddr> {
     match SlicedPacket::from_ip(&packet) {
         Ok(packet) => match packet.net {
             Some(net) => match net {
-                etherparse::InternetSlice::Ipv4(ipv4) => match &ipv4.header().destination_addr().to_address() {
+                etherparse::InternetSlice::Ipv4(ipv4) => match &ipv4.header().destination_addr().into_address() {
                     Ok(addr) => Ok(addr.clone()),
                     Err(err) => Err(anyhow::anyhow!("failed to parse IPv4 address: {}", err))
-                }
+                },
                 etherparse::InternetSlice::Ipv6(_) => {
                     Err(anyhow::anyhow!("IPv6 is not supported"))
+                },
+                etherparse::InternetSlice::Arp(_) => {
+                    Err(anyhow::anyhow!("ARP is not supported"))
                 }
             },
             None => Err(anyhow::anyhow!("failed to parse IP packet: missing network layer"))
