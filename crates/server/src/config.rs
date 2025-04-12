47189adc32b600159a4213e6a712e51857c5ee21
JKearnsl <boyarshinovn@gmail.com>
2025-04-12 12:46:21 +0300

    add buf runtime params

--- crates/server/src/config.rs
+++ crates/server/src/config.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 use shared::keys::handshake::SecretKey;
 
 #[derive(Serialize, Deserialize)]
-pub struct General {
+pub struct GeneralConfig {
     pub host: String,
     pub port: u16,
     pub secret_key: SecretKey,
@@ -12,61 +12,40 @@ pub struct General {
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct Interface {
+pub struct InterfaceConfig {
     pub name: String,
     pub mtu: u16,
     pub address: IpAddr,
     pub prefix: u8
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct Runtime {
+#[derive(Serialize, Deserialize, Clone)]
+pub struct RuntimeConfig {
     pub workers: usize,
-    pub sender_buf_size: usize,
+    pub so_rcvbuf: usize,
+    pub so_sndbuf: usize,
+    pub out_udp_buf: usize,
+    pub out_tun_buf: usize,
+    pub handshake_buf: usize,
+    pub data_udp_buf: usize,
+    pub data_tun_buf: usize,
     pub session_ttl: usize, // sec
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct Redirect {
+pub struct RedirectConfig {
     pub enabled: bool,
+    pub ipv4_forwarding: bool,
     pub interfaces: Vec<String>,
 }
 
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
-    pub general: General,
-    pub interface: Interface,
-    pub runtime: Runtime,
-    pub redirect: Option<Redirect>,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Self {
-            general: General {
-                host: IpAddr::from([0, 0, 0, 0]).to_string(),
-                port: 26256,
-                secret_key: SecretKey::generate_x25519(),
-                storage: PathBuf::from("database"),
-            },
-            interface: Interface {
-                name: "holynet0".into(),
-                mtu: 1420,
-                address: IpAddr::from([10, 8, 0, 0]),
-                prefix: 24,
-            },
-            runtime: Runtime {
-                workers: 0, // auto
-                sender_buf_size: 1000,
-                session_ttl: 0, // turn off
-            },
-            redirect: Some(Redirect {
-                enabled: false,
-                interfaces: vec![],
-            })
-        }
-    }
+    pub general: GeneralConfig,
+    pub interface: InterfaceConfig,
+    pub runtime: Option<RuntimeConfig>,
+    pub redirect: Option<RedirectConfig>
 }
 
 impl Config {
@@ -79,5 +58,66 @@ impl Config {
         let config = toml::to_string(self)?;
         std::fs::write(path, &config)?;
         Ok(())
+    }
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            general: GeneralConfig::default(),
+            interface: InterfaceConfig::default(),
+            runtime: Some(RuntimeConfig::default()),
+            redirect: Some(RedirectConfig::default())
+        }
+    }
+}
+
+impl Default for GeneralConfig {
+    fn default() -> Self {
+        Self {
+            host: IpAddr::from([0, 0, 0, 0]).to_string(),
+            port: 26256,
+            secret_key: SecretKey::generate_x25519(),
+            storage: PathBuf::from("database"),
+        }
+    }
+}
+
+impl Default for InterfaceConfig {
+    fn default() -> Self {
+        Self {
+            name: "holynet0".into(), // todo from available interface number
+            mtu: 1420,
+            address: IpAddr::from([10, 8, 0, 0]),
+            prefix: 24,
+        }
+    }
+}
+
+impl Default for RuntimeConfig {
+    fn default() -> Self {
+        Self {
+            workers: 0, // auto
+            so_rcvbuf: 1024 * 1024 * 1024,
+            so_sndbuf: 1024 * 1024 * 1024,
+            out_udp_buf: 1000,
+            out_tun_buf: 1000,
+            handshake_buf: 1000,
+            data_udp_buf: 1000,
+            data_tun_buf: 1000,
+            session_ttl: 0, // turn off
+        }
+    }
+}
+
+impl Default for RedirectConfig {
+    fn default() -> Self {
+        Self {
+            enabled: false,
+            ipv4_forwarding: true, // todo
+            interfaces: vec![
+                // todo add default interface
+            ]
+        }
     }
 }
--- crates/shared/src/connection_config.rs
+++ crates/shared/src/connection_config.rs
@@ -9,46 +9,43 @@ use serde::{Deserialize, Serialize};
 use std::path::Path;
 
 #[derive(Serialize, Deserialize)]
-pub struct General {
+pub struct GeneralConfig {
     pub host: String,
     pub port: u16,
     pub alg: Alg
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct Credentials {
+#[derive(Serialize, Deserialize, Clone)]
+pub struct CredentialsConfig {
     pub private_key: SecretKey,
     pub pre_shared_key: SecretKey,
     pub server_public_key: PublicKey
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct Interface {
+pub struct InterfaceConfig {
     pub name: String,
     pub mtu: u16
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct Runtime {
+#[derive(Serialize, Deserialize, Clone)]
+pub struct RuntimeConfig {
     pub handshake_timeout: u64, // ms
     pub keepalive: Option<u64>, // sec
-}
-
-impl Default for Runtime {
-    fn default() -> Self {
-        Self {
-            handshake_timeout: 1000,
-            keepalive: Some(5)
-        }
-    }
+    pub so_rcvbuf: usize,
+    pub so_sndbuf: usize,
+    pub out_udp_buf: usize,
+    pub out_tun_buf: usize,
+    pub data_udp_buf: usize,
+    pub data_tun_buf: usize,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct ConnectionConfig {
-    pub general: General,
-    pub credentials: Credentials,
-    pub interface: Option<Interface>,
-    pub runtime: Option<Runtime>
+    pub general: GeneralConfig,
+    pub credentials: CredentialsConfig,
+    pub interface: Option<InterfaceConfig>,
+    pub runtime: Option<RuntimeConfig>
 }
 
 
@@ -81,5 +78,20 @@ impl ConnectionConfig {
             |err| anyhow::Error::from(err)
         )?;
         Ok(STANDARD_NO_PAD.encode(&bytes))
+    }
+}
+
+impl Default for RuntimeConfig {
+    fn default() -> Self {
+        Self {
+            handshake_timeout: 3000, // 3sec
+            keepalive: Some(5), // 5sec
+            so_rcvbuf: 1024 * 1024 * 1024,
+            so_sndbuf: 1024 * 1024 * 1024,
+            out_udp_buf: 1000,
+            out_tun_buf: 1000,
+            data_udp_buf: 1000,
+            data_tun_buf: 1000,
+        }
     }
 }
